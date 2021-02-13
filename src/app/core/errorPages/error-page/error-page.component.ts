8a1585940cab752db7d2cdaf29e38cadd706bfb0
Prajeet-Shrestha <prazzeettstha@gmail.com>
2021-02-13 16:09:42 +0545

    Error Pages Added

--- src/app/core/errorPages/error-page/error-page.component.ts
+++ src/app/core/errorPages/error-page/error-page.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-error-page',
   templateUrl: './error-page.component.html',
-  styleUrls: ['./error-page.component.css']
+  styleUrls: ['./error-page.component.css'],
 })
 export class ErrorPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  @Input() noResult: boolean = false;
+  ngOnInit(): void {}
 }
